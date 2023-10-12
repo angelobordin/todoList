096a7d3f66271149ef8c0b45fc0fb530d96928f9
AngeloBordin <angelombordin@gmail.com>
2023-10-12 15:04:30 -0300

    create user model

---
+++ src/main/java/br/com/angelobordin/todolist/user/UserModel.java
@@ -0,0 +1,12 @@
+package br.com.angelobordin.todolist.user;
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+
+    private String name;
+    private String username;
+    private String password;
+
+}
