ed726823e5eb6abb22227657da5c17648cf4e945
AngeloBordin <angelombordin@gmail.com>
2023-10-12 17:42:51 -0300

    create filter to validate user information

---
+++ src/main/java/br/com/angelobordin/todolist/filter/FilterTaskAuth.java
@@ -0,0 +1,22 @@
+package br.com.angelobordin.todolist.filter;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class FilterTaskAuth extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain cb) throws ServletException, IOException {
+        cb.doFilter(req, res);
+    }
+
+}
